3e43b7ad031df90739ad35574bac9b7e39e8a25a
rumi.kg <rumi.kg@f7a59cef-1038-38d6-8abd-5f9aa8287e2c>
2010-06-13 13:09:21 +0000

    Changes to 404 page and dont indent content on forhandlere.

--- trunk/pages/404.php
+++ trunk/pages/404.php
@@ -1,1 +1,5 @@
-Siden du leder efter findes ikke.
+<h1>Du er kommet på vildspor</h1>
+<p>
+    Siden du leder efter findes desværre ikke. Hvis du har lyst kan du gå
+    tilbage til <a href="/" title="Gå til forsiden">forsiden</a>.
+</p>
